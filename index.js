e97ccf64276dd549249eb39d5501e65c0abaced5
laurenciopaez <laurencio314gmail.com>
2024-01-02 00:03:25 -0300

    vercel

--- index.js
+++ index.js
@@ -82,3 +82,5 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+module.exports = app;
