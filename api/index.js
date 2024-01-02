acff2a196a0bbb5e3cf77b577b900f5b03413d89
laurenciopaez <laurencio314gmail.com>
2024-01-02 00:20:46 -0300

    folder fix

--- api/index.js
+++ api/index.js
@@ -1,24 +1,8 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require("express");
-var app = express();
+var router = express.Router();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "../views/index.html");
-});
-
-app.get("/api/:date?", function (req, res) {
+router.get("/:date?", function (req, res) {
   //guardo en una variable los datos que vienen por la solicitud http
   const dateParam = req.params.date;
 
@@ -74,13 +58,9 @@ app.get("/api/:date?", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
+router.get("/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
 
-module.exports = app;
+module.exports = router;
